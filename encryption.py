a48d9512134547f7d4feec29cbfabb260a0fcaf9
F.Wulf <118770194+Wulf-03@users.noreply.github.com>
2024-02-27 14:33:04 +0100

    Adding the lists
    Changed the elements to strings

--- encryption.py
+++ encryption.py
@@ -4,8 +4,8 @@
 ### General structure of the programm
 
 counter = 0
-disk1 = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-disk1 = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, ,]
+disk1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+disk2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 while counter == 0:      #starting the loop
     
     choice = input("encryption (e), decryption (d), close program (any key): ")
