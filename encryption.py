5bbf6d2e56f88b255159566cb72f9fca3ae004f8
F.Wulf <118770194+Wulf-03@users.noreply.github.com>
2024-02-27 15:25:23 +0100

    Completed the lists
    The lists were completet so the program would understand full texts. Also the program description was specified!

--- encryption.py
+++ encryption.py
@@ -1,14 +1,20 @@
-### Program for encryption and decryption
+### Program for encryption and decryption of Texts
 ### Ask whether it should be encrypted or decrypted
 ### Program should be executed as a loop
 ### General structure of the programm
 
 counter = 0
-disk1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-disk2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+disk1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "!", "?"]
+
+disk2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "!", "?", " "]
+
 while counter == 0:      #starting the loop
     
-    choice = input("encryption (e), decryption (d), close program (any key): ")
+    choice = input("encryption (e), decryption (d), close program (any key): ")  
 
     if choice == "e":  #choice encryption 
         code = input("Please enter your password ") #enter password
