bb95d644b89054c5ccfb9f72ffaf33b18503d56a
F.Wulf <118770194+Wulf-03@users.noreply.github.com>
2024-02-27 13:55:26 +0100

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -3,6 +3,8 @@
 ### Program should be executed as a loop
 ### General structure of the programm
 choice = input("encryption (e) , decryption (d) , close program (any key)")
+i = 0
+while i == 0:
 
 if choice == "e":  #choice encryption 
     code = input("Please enter your password ") #enter password
@@ -11,6 +13,5 @@ elif choice == "d":     #choice decryption
     code = input("Please enter your passwort")
     
 else:   #choice closing program
+    i = i + 1
     print("Closing program ... ")
-
-        
