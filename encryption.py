c300c4e20061e13a5f5aa8332201de6aea90404d
Raphael Bölts <119746073+LaCaBCMD@users.noreply.github.com>
2024-02-27 15:29:45 +0100

    Shift / + int

--- encryption.py
+++ encryption.py
@@ -6,15 +6,19 @@
 counter = 0
 disk1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 disk2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+password_part2 = disk2[::2]
+print (disk2)
+
 while counter == 0:      #starting the loop
     
     choice = input("encryption (e), decryption (d), close program (any key): ")
 
     if choice == "e":  #choice encryption 
-        code = input("Please enter your password ") #enter password
+        code = int(input("Please enter your password ")) #enter password
     
     elif choice == "d":     #choice decryption
-        code = input("Please enter your passwort ")
+        code = int(input("Please enter your passwort "))
     
     else:   #choice closing program
         counter = counter + 1   #ending the loop
