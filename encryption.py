1519cab5beff692ea8bed0482e24bfebc0a4d1f2
NicklasFi <nicklaspfischer@gmail.com>
2024-02-29 16:35:34 +0100

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -7,40 +7,41 @@ counter = 0
 
 disk1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
          "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
-         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "!", "?", " "]
+         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "!", "?", " "] # Disk 1
 
 disk2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
          "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
-         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "!", "?", " "]
+         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "!", "?", " "] # Disk 2
 
-while counter == 0:      #starting the loop
+while counter == 0:      # Starting the loop
     
-    choice = input("encryption (e), decryption (d), close program (any key): ")  
+    choice = input("encryption (e), decryption (d), close program (any key): ")  # Choice between encryption, decryption or close programm 
 
-    if choice == "e":  #choice encryption 
-        encrypteven = []
-        encryptodd = []
-        d1indexmarker = []
-        d2indexmarker = []
+
+    if choice == "e":  # Choice encryption 
+        encrypteven = [] # Even signs from text input 
+        encryptodd = [] # Odd signs from text input 
+        d1indexmarker = [] # Marks index of disk 1 where disk 1 and index cross
+        d2indexmarker = [] # Marks index of disk 2 where disk 2 and index cross
         
         while True:
-            code = int(input("Please enter your password ")) #enter password.
-            if code >=0 and ValueError = False:   # Only full and positive numbers check. 
-                break       # If full number = exiting loop.
-            else:           # else ask give an error message and ask for an new input.
+            code = int(input("Please enter your password ")) # Enter password
+            if code >=0 and ValueError == False:   # Only full and positive numbers check
+                break       # If full number = exiting loop
+            else:           # Else ask give an error message and ask for an new input
                 print("Invalid input. Do not use negative numbers ")
         
-        text = list(str(input("Please enter the text you want to encrypt: ")))
+        text = list(str(input("Please enter the text you want to encrypt: "))) # Entering your selected text
         
-        for y in range(len(text)): #Dividing the text into two lists for better protection
+        for y in range(len(text)): # Dividing the text into two lists for better protection
             if y % 2:
-                encrypteven.append(text[y])
+                encrypteven.append(text[y]) # Even signs in text input 
             else:
-                encryptodd.append(text[y])
-        for z in range(len(text)/2):    #Creating a list to keep track of the indexes
-            d1indexmarker.append(disk1.index(encrypteven[z]))
+                encryptodd.append(text[y]) # Odd signs in text input
+        for z in range(len(text)/2):    # Creating a list to keep track of the indexes
+            d1indexmarker.append(disk1.index(encrypteven[z])) 
             d2indexmarker.append(disk2.index(encryptodd[z]))
-        for i in range(code):   #Shifting the disks to encrypt the text
+        for i in range(code):   # Shifting the disks to encrypt the text
             disk1.append(disk1.pop(0))
         for j in range(code^2):
             disk2.append(disk2.pop(0))
@@ -48,9 +49,9 @@ while counter == 0:      #starting the loop
         
         
 
-    elif choice == "d":     #choice decryption
-        code = int(input("Please enter your passwort "))
+    elif choice == "d":     # Choice decryption
+        code = int(input("Please enter your password ")) # Entering password
     
-    else:   #choice closing program
-        counter = counter + 1   #ending the loop
-        print("Closing program ... ")
+    else:   # Choice closing program
+        counter = counter + 1   # Ending the loop
+        print("Closing program ... ") # Programm closed
